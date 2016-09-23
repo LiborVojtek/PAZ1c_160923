c283f39c5a08688afa3c48bb8748e642ede9dc7f
student <student@158.197.35.86>
2016-09-23 08:08:46 +0200

    Merge origin/master
    Conflicts:
    	src/sk/upjs/paz1c/Cviko01.java

--- src/sk/upjs/paz1c/cvicenie01.java
+++ src/sk/upjs/paz1c/cvicenie01.java
@@ -16,7 +16,7 @@ public class cvicenie01 {
      */
     public static void main(String[] args) {
         for (int i = 0; i< 5; i++)
-            System.out.println("Dobrý deň.");
+            System.out.println("Dobrý " + i + " deň.");
     }
     
 }
